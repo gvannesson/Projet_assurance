7afe5a01a2f4c8ed3056ec5b80bd5aa88710901e
gvannesson <g.vannesson@gmail.com>
2025-01-16 15:18:34 +0100

    commit du jeudi après midi

--- classe_p.py
+++ classe_p.py
@@ -8,9 +8,15 @@ class DropFeatureSelector(BaseEstimator, TransformerMixin):
     def fit(self, X, y = None):
         return self
     
+    def get_feature_names_out(self, feature_names_out):
+        return feature_names_out
+    
+    def fit_transform(self, X, y = None, **fit_params):
+        return super().fit_transform(X, y, **fit_params)
+    
     def transform(self, X):
         X_cop= X.copy()
-        bins = [0,18,25,30,35,40,50,60]
+        bins = [0,18,25,30,35,40,50,1000]
         labels=['sous poids','poids normal','surpoids','obésité modérée','obésité sévère','obésité morbide','obésité massive']
         X_cop['BMI_cat']=pd.cut(X_cop['bmi'], bins=bins, labels=labels, right=False)
         X_cop= X_cop.drop(['bmi'], axis=1)
