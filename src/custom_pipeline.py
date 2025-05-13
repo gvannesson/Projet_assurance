7f0516a651a7c9b26ef8c2c97d058fbf555c9412
gvannesson <g.vannesson@gmail.com>
2025-05-13 09:19:44 +0200

    pytest MAJ day 2

---
+++ src/custom_pipeline.py
@@ -0,0 +1,3 @@
+from data_csv import data
+
+print(data)
---
+++ tests/test_data_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+import sys
+import os
+sys.path.append(os.getcwd())
+print('hello')
+print(os.getcwd())
+
+from src.data_csv import data
+print(data)
