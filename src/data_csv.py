870bdbd0d3e68d883b3c0f3daaea726156fd2c1f
gvannesson <g.vannesson@gmail.com>
2025-05-14 16:29:39 +0200

    pytest MAJ day 3

--- src/data_csv.py
+++ src/data_csv.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-data = pd.read_csv('dataset.csv')
+
+def readcsv(csv):
+    data = pd.read_csv(csv)
+    return data
 
 def drop_dup(data):
     data.drop_duplicates(inplace=True, ignore_index=True)
     return data
 
 
-drop_dup(data)
+drop_dup(readcsv('dataset.csv'))
