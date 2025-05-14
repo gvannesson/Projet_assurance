24b878f4bb5593fb509e96c854bb58a1562de15e
gvannesson <g.vannesson@gmail.com>
2025-05-14 13:34:57 +0200

    pytest MAJ day 3

--- tests/test_custom_pipeline.py
+++ tests/test_custom_pipeline.py
@@ -3,5 +3,8 @@ import os
 sys.path.append(os.getcwd())
 
 from src.custom_pipeline import DropFeatureSelector
+from src.drop_charges import grid_search
+
+
 
 
--- tests/test_data_csv.py
+++ tests/test_data_csv.py
@@ -3,8 +3,6 @@ import pandas as pd
 import sys
 import os
 sys.path.append(os.getcwd())
-print('hello')
-print(os.getcwd())
 
 from src.data_csv import data
 
